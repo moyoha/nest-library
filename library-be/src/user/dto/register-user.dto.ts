cdd3c99a02c1a79c8b1036348cbb3dbcdec2e606
v_mayonghua <v_mayonghua@baidu.com>
2025-03-07 19:12:02 +0800

    添加用户模块初始化代码

---
+++ library-be/src/user/dto/register-user.dto.ts
@@ -0,0 +1,9 @@
+import { IsNotEmpty, MinLength } from 'class-validator';
+
+export class RegisterUserDto {
+  @IsNotEmpty({ message: 'Username cannot be empty' })
+  username: string;
+  @IsNotEmpty({ message: 'Password cannot be empty' })
+  @MinLength(6, { message: 'Password must be at least 6 characters long' })
+  password: string;
+}

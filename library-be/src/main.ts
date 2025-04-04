afcc2b3f866ddd27b2b0140e5ae77ccf5f7e798a
v_mayonghua <v_mayonghua@baidu.com>
2025-04-04 17:04:44 +0800

    完成登录注册页面

--- library-be/src/main.ts
+++ library-be/src/main.ts
@@ -6,6 +6,7 @@ import { join } from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.enableCors();
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
   app.useStaticAssets(join(__dirname, '../uploads'), { prefix: '/uploads' });
   await app.listen(process.env.PORT ?? 3000);
